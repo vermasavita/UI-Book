adb95acc38fe89e1a3b750eb90ad1c4b51a2ffab
Savita Verma <svitaverma10@gmail.com>
2022-02-08 14:26:36 +0530

    fix: navbar

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const toggleBtnTwo = document.querySelector(".toggle-btn");
 const rightNavTwo = document.querySelector(".right-nav");
+const docContainer = document.querySelector(".doc-container");
 
 toggleBtnTwo.addEventListener("click", () => {
   rightNavTwo.classList.toggle("active");
+  docContainer.classList.toggle("body-margin-top");
   console.log(rightNavTwo);
 });
